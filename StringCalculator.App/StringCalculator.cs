c59e6a607c1ba7797b6f80774ea04503d87b2865
Adam Smith <hagansmith@gmail.com>
2018-03-03 10:51:39 -0600

    single number tests pass

---
+++ StringCalculator.App/StringCalculator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator.App
+{
+    public class Calculator
+    {
+        public int Add (string numbers)
+        {
+            int numInt;
+            
+            bool result = int.TryParse(numbers, out numInt);
+            if (result)
+            {
+                return numInt;
+            }
+            else
+            {
+              return numInt = 0;
+            }
+            
+        }
+    }
+
+    //public class Add
+    //{
+    //    public string Numbers { get; set; }
+    //}
+
+}
---
+++ StringCalculator.Tests/StringCalculationTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using StringCalculator.App;
+
+namespace StringCalculator.Tests
+{
+
+    [TestFixture]
+    public class StringCalculatorTests
+    {
+        [Test]
+        public void Entering_No_Numbers_Returns_0()
+        { 
+            var numbers = "";
+            var stringCalculator = new Calculator();
+
+            var result = stringCalculator.Add(numbers);
+ 
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void Entering_1_Number_Returns_That_Number()
+        {
+            var numbers = "1";
+            var stringCalculator = new Calculator();
+
+            var result = stringCalculator.Add(numbers);
+
+            Assert.AreEqual(1, result);
+        }
+
+        [Test]
+        public void Entering_The_Number_2_Returns_2()
+        {
+            var numbers = "2";
+            var stringCalculator = new Calculator();
+
+            var result = stringCalculator.Add(numbers);
+
+            Assert.AreEqual(2, result);
+        }
+    }
+}
